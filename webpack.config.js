f8037abac1e3b451c7d47e5d4b1c618e9b53a1af
sam bacha <sam@freighttrust.com>
2021-02-28 13:42:32 -0800

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+// @file webpack config
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
+
+module.exports = function () {
+
+  const config = {
+    entry: {
+      app: './src/app.js',
+    },
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: '[name].js',
+    },
+    resolve: {
+      modules: ['./node_modules']
+    },
+    plugins: [
+      new CopyWebpackPlugin([
+        {
+          from: './src/assets'
+        },
+      ]),
+    ]
+  }
+
+  return config
+}
